fd974dd0a07d5e55c035856e273052857634e31c
arifulhasantotul <mdahtotul@gmail.com>
2022-11-22 12:53:06 +0600

    console design

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const colors = require("colors");
 const models = require("./models");
 const mergedResolvers = require("./Schema/resolvers");
 
-const port = process.env.PORT || 4000;
+// initialization
 const app = express();
+const port = process.env.PORT || 4000;
 
+// database connection
 connectDB();
 
+// running async apollo server
 async function runApolloServer() {
   const server = new ApolloServer({
     typeDefs,
@@ -26,9 +29,29 @@ async function runApolloServer() {
 
 runApolloServer();
 
+// getting current time
+const today = new Date();
+const time =
+  today.getHours() +
+  ":" +
+  today.getMinutes() +
+  ":" +
+  today.getSeconds() +
+  "sec";
+
+// stroing current server url
+const server_string =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:${port}/`
+    : process.env.prod_server;
+
+// watching server on console with some styles
 app.listen({ port: port }, () =>
   console.log(
-    `🚀 GraphQL ${process.env.NODE_ENV} Server ready at ` +
-      `http://localhost:${port}/graphql`.yellow.underline
+    `==========================================================================================
+    ${time.red.bold} 🚀 GraphQL ${process.env.NODE_ENV} sever endpoint ` +
+      `${server_string}/graphql`.yellow.underline +
+      `\n                📢 ${process.env.NODE_ENV} server url ${server_string}` +
+      `   ➡️  port:${port}`.green
   )
 );
