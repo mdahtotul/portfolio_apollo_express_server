ffdb9da187c0f47c43371e4346bde4c9f3ae825c
mdahtotul <mdahtotul@gmail.com>
2023-06-12 10:31:47 +0600

    error handled for category resolvers

--- Schema/resolvers/categoryResolvers.js
+++ Schema/resolvers/categoryResolvers.js
@@ -1,51 +1,119 @@
+const { AuthenticationError } = require("apollo-server-express");
 const models = require("../../models");
 
 const categoryResolvers = {
   Query: {
     listCategory: async (parent, args, context) => {
-      const categories = await models.DB_Category.find({})
-        .collation({ locale: "en_US", strength: 2 })
-        .sort({ name: 1 });
-      return categories;
+      try {
+        const categories = await models.DB_Category.find({})
+          .collation({ locale: "en_US", strength: 2 })
+          .sort({ name: 1 });
+        return categories;
+      } catch (err) {
+        throw new Error(err);
+      }
     },
 
     getCategory: async (parent, args, context) => {
-      const category = await models.DB_Category.findById(args?.id);
-      return category;
+      try {
+        const category = await models.DB_Category.findById(args?.id);
+        return category;
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 
   Category: {
     projects: async (parent, args, context) => {
-      return await models.DB_Project.find({ _id: { $in: parent.projectsId } });
+      try {
+        return await models.DB_Project.find({
+          _id: { $in: parent.projectsId },
+        });
+      } catch (err) {
+        throw new Error(err);
+      }
     },
     blogs: async (parent, args, context) => {
-      return await models.DB_Blog.find({ _id: { $in: parent.blogsId } });
+      try {
+        return await models.DB_Blog.find({ _id: { $in: parent.blogsId } });
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 
   Mutation: {
-    createCategory: async (parent, args, context) => {
-      const newCategory = new models.DB_Category({
-        name: args.input.name,
-      });
-      return await newCategory.save();
-    },
-    updateCategory: async (parent, args, context) => {
-      const updateCategory = new models.DB_Category({
-        _id: args.id,
-        name: args.input.name,
-      });
-      return await models.DB_Category.findOneAndUpdate(
-        { _id: args.id },
-        updateCategory,
-        {
-          new: true,
+    createCategory: async (parent, args, { req, res }) => {
+      try {
+        if (!req.isAuth || !req.userId || req.userRole !== "Admin") {
+          throw new AuthenticationError("Unauthenticated! ⛔");
         }
-      );
+
+        const existCategory = await models.DB_Category.findOne({
+          name: args.input.name,
+        });
+
+        if (existCategory) {
+          throw new Error("Category already exist!");
+        }
+
+        const newCategory = new models.DB_Category({
+          name: args.input.name,
+        });
+        return await newCategory.save();
+      } catch (err) {
+        throw new Error(err);
+      }
     },
-    deleteCategory: async (parent, args, models) => {
-      return await models.DB_Category.findByIdAndDelete(args.id);
+    updateCategory: async (parent, args, { req, res }) => {
+      try {
+        if (!req.isAuth || !req.userId || req.userRole !== "Admin") {
+          throw new AuthenticationError("Unauthenticated! ⛔");
+        }
+
+        const updateCategory = new models.DB_Category({
+          _id: args.id,
+          name: args.input.name,
+        });
+        return await models.DB_Category.findOneAndUpdate(
+          { _id: args.id },
+          updateCategory,
+          {
+            new: true,
+          }
+        );
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    deleteCategory: async (parent, args, { req, res }) => {
+      try {
+        if (!req.isAuth || !req.userId || req.userRole !== "Admin") {
+          throw new AuthenticationError("Unauthenticated! ⛔");
+        }
+
+        const category = await models.DB_Category.findByIdAndDelete(args.id);
+
+        // NOTE: removing categoriesId from projects
+        if (category?.projectsId?.length > 0) {
+          await models.DB_Project.updateMany(
+            { _id: { $in: category.projectsId } },
+            { $pull: { categoriesId: category._id } }
+          );
+        }
+        // NOTE: removing categoriesId from blogs
+        if (category?.blogsId?.length > 0) {
+          await models.DB_Blog.updateMany(
+            { _id: { $in: category.blogsId } },
+            { $pull: { categoriesId: category._id } }
+          );
+        }
+
+        return category;
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 };
